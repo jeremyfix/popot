eaae7c067023f8b3ecabb6773006083f3b1c4666
Jeremy Fix <jeremy.fix@gmail.com>
2015-04-21 09:22:49 +0200

    updating the licence to GPL

--- src/standard_pso.h
+++ src/standard_pso.h
@@ -1,24 +1,23 @@
 /*
-  This file is part of popot.
+    This file is part of popot.
+    Copyright (C) 2014  Jeremy Fix, CentraleSupelec
 
-  Copyright (C) 2014, Jeremy Fix, CentraleSupelec
+    Author : Jeremy Fix
 
-  Author : Jeremy Fix
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-  popot is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-  popot is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  You should have received a copy of the GNU Lesser General Public License
-  along with popot.  If not, see <http://www.gnu.org/licenses/>.
-
-  Contact : Jeremy.Fix@centralesupelec.fr
+    Contact : Jeremy.Fix@centralesupelec.Fr
 */
 
 #ifndef POPOT_STANDARD_PSO
