b048d2578e29f9acc96a60ace43c534552779692
Jeremy Fix <jeremy.fix@gmail.com>
2015-05-20 10:15:24 +0200

    Introducing Reinforcement learning problems

---
+++ src/problems_rl.h
@@ -0,0 +1,50 @@
+/*
+    This file is part of popot.
+    Copyright (C) 2014  Jeremy Fix, CentraleSupelec
+
+    Author : Jeremy Fix
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact : Jeremy.Fix@centralesupelec.Fr
+*/
+
+#ifndef POPOT_PROBLEMS_RL_H
+#define POPOT_PROBLEMS_RL_H
+
+namespace popot {
+
+  namespace problems {
+    /**
+     * Problems in reinforcement learning
+     **/
+    namespace rl {
+
+      namespace mountain_car {
+
+      }
+
+      namespace inverted_pendulum {
+
+      }
+
+      namespace acrobat {
+      }
+
+    }
+  }
+}
+
+
+#endif
